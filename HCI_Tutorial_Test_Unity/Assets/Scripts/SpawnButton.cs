afa470ebebdc76c56e985049a0ec7259bd31209a
Jocelyn Putnam <jocelyn.putnam@colostate.edu>
2024-04-03 23:17:58 -0600

    Spawn Button Functional!
    Spawn button for cube, sphere, cylinder, and pyramid!. Script for spawnbutton to hide the shapes at first is in the first Cube object's components

--- HCI_Tutorial_Test_Unity/Assets/Scripts/SpawnButton.cs
+++ HCI_Tutorial_Test_Unity/Assets/Scripts/SpawnButton.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnButton : MonoBehaviour
 {
+    public GameObject Cube;
+    public GameObject Cube2;
+    public GameObject Cube3;
+    public GameObject Sphere;
+    public GameObject Cylinder;
+    public GameObject Pyramid;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cube.SetActive(false);
+        Cube2.SetActive(false);
+        Cube3.SetActive(false);
+        Sphere.SetActive(false);
+        Cylinder.SetActive(false);
+        Pyramid.SetActive(false);
     }
 
+    
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     
 }
