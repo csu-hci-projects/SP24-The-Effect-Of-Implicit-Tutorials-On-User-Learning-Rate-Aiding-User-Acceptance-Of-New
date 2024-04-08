5dc373f2f30bd33530ef5e7af22be04358161ab6
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2024-04-08 13:28:57 -0600

    scene changing

---
+++ HCI_Tutorial_Test_Unity/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    /*
+     noTutorial = notut
+     standardTutorial = stut
+     highlightTutorial = htut
+     
+     */
+
+    public int NextSceneIndex = 0;
+    public DataFileWriter dfw;
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadScene(NextSceneIndex);
+    }
+
+    public void SwitchScenesInitially()
+    {
+        string TrialType = dfw.TrialType;
+        if (TrialType == "notut")
+        {
+            //send to first trial for no tutorial
+            SceneManager.LoadScene(NextSceneIndex); //temp
+        }
+        if (TrialType == "stut")
+        {
+            //send to first trial for standard tutorial
+            SceneManager.LoadScene(NextSceneIndex); //temp
+        }
+        if (TrialType == "htut")
+        {
+            //send to first trial for highlighted tutorial
+            SceneManager.LoadScene(NextSceneIndex); //temp
+        }
+    }
+}
